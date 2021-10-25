e0abace7a5cdce7e0747b124021dccc26bd1c2bf
D3pari <christian.pratama@ninjavan.co>
2021-10-25 14:51:35 +0700

    Simpan semua file pertemuan 1

---
+++ Depari-W1.py
@@ -0,0 +1,27 @@
+#print adalah keyword untuk mencetak teks
+#print("Hello,Malik!")
+
+
+#tidak perlu di otak atik
+
+#x1 = 100
+#x2 = 200
+# y = "Hello,World!"
+
+#print(x1 * x2)
+#print(y)
+
+#print(type(x1))
+
+#print(x1**x2)
+
+#n = 2
+#o = 6
+
+#print(n*o)
+
+#print((type(n)))
+
+n=2*6/3
+
+print
