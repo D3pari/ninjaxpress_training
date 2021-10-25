fca9f05902d7d255cdba8319713be4b5665483b6
D3pari <christian.pratama@ninjavan.co>
2021-10-25 16:28:28 +0700

    Save W2 Python course

---
+++ Depari-W2.py
@@ -0,0 +1,69 @@
+# name = input("Enter name: ")
+# print("Your name is ",  +name)
+
+# x = "Hello world!"
+
+# print(len(x))
+# print(x[0:4])
+# print(x[1:4])
+
+# x = x.replace("world","friend")
+# print(x)
+
+# # a = "Hello"
+# # b = "World!"
+# # c = a + " " + b
+# print(c)
+
+# a = "Siap"
+# b = 86
+# c = a + " "+ str(b)
+# print(c)
+
+# nama = "Depari"
+# alamat = "Batam"
+
+# my_profile = "Nama saya {} dan saat ini saya bekerja di {}.".format(nama,alamat)
+
+# print(my_profile)
+
+# x = "Indonesia AI"
+
+# print(x[2:4])
+# print(len(x))
+
+# p = "True"
+# print(type(p))
+
+# Booleans
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# a = 10
+# b = 9
+
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than b")
+
+# List
+# my_list = []
+# my_list.append(1)
+# my_list.append(2)
+# my_list.append(3)
+# my_list.append("my string")
+# my_list.append(10 > 9)
+
+# print(my_list)
+
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+
